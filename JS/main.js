1055e68571740a40aa684a58097caf8d5e459fa6
MostafaBelo <mostafaelfaggal@gmail.com>
2023-11-13 22:18:31 +0200

    move the test cases to main.js

--- JS/main.js
+++ JS/main.js
@@ -2,6 +2,9 @@ import { regs, RAM } from "./data.js";
 import ui from "./gui.js";
 import { convertToHexAndBinary } from "./dataConversions.js";
 
+regs[5] = 21;
+regs[6] = 22;
+regs[7] = 23;
 RAM.write1(8001, 21);
 RAM.write1(8017, 4);
 RAM.write1(8008, 106);
