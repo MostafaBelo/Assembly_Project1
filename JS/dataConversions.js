985d9134b7cd0d99d0105edd264a219b55f3748e
Raef-Hany <98807473+Raef-Hany@users.noreply.github.com>
2023-11-19 20:12:20 +0200

    negative values fixed in binary conversion

--- JS/dataConversions.js
+++ JS/dataConversions.js
@@ -1,4 +1,18 @@
 export function convertToHexAndBinary(decimalValue, valueSize = 32) {
+	// Convert to 2's complement
+	let sign = decimalValue >> (valueSize - 1);
+	console.log(sign, decimalValue, valueSize);
+	if (sign == 1) {
+		let temp = 1;
+		for (let i = 0; i < 32 - valueSize - 1; i++) {
+			temp = temp << 1;
+			temp = temp | 1;
+		}
+		temp = temp << valueSize;
+		decimalValue = decimalValue | temp;
+	}
+	
+	
 	const hexValue =
 		"0x" +
 		decimalValue
@@ -7,4 +21,5 @@ export function convertToHexAndBinary(decimalValue, valueSize = 32) {
 			.padStart(valueSize / 4, "0");
 	const binaryValue = "0b" + decimalValue.toString(2).padStart(valueSize, "0");
 	return [decimalValue, hexValue, binaryValue];
+
 }
