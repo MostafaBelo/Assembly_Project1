17dbd12d39bc619877ac882d49863a2f4f6f411c
MostafaBelo <mostafaelfaggal@gmail.com>
2023-11-19 23:52:12 +0200

    removed regs and memory test cases from main

--- Code/JS/main.js
+++ Code/JS/main.js
@@ -5,15 +5,15 @@ import ui from "./gui.js";
 import { Parser } from "./parser.js";
 //import { validate } from "webpack";
 
-regs.write("x5", 21);
-regs.write("t1", 22);
-regs.write(7, 21);
-regs.write("x0", 21);
-regs.write("zero", 21);
+// regs.write("x5", 21);
+// regs.write("t1", 22);
+// regs.write(7, 21);
+// regs.write("x0", 21);
+// regs.write("zero", 21);
 
-RAM.write1(8001, 21);
-RAM.write1(8017, 4);
-RAM.write1(8008, 106);
+// RAM.write1(8001, 21);
+// RAM.write1(8017, 4);
+// RAM.write1(8008, 106);
 
 flow.setupUI(ui);
 flow.setParser(new Parser());
